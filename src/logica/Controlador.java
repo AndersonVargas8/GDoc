e6fb77478a720eb60c9ab1605e021632f3703763
AndersonVargas8 <anvargasa@unal.edu.co>
2021-04-12 12:45:16 -0500

    Principio de un controlador

---
+++ src/logica/Controlador.java
@@ -0,0 +1,42 @@
+package logica;
+import java.util.*;
+import java.io.*;
+import datos.*;
+/**
+ *La clase Controlador permite gestionar las funcionalidades correspondientes a las carpetas y sus documentos.
+ * @author Anderson
+ */
+public class Controlador {
+    public static ListaEncadenadaSimple<Carpeta> sec = new ListaEncadenadaSimple<>();
+    public static final String SEPARATOR=";";
+   public static final String QUOTE="\"";
+   public static void main(String[] args) throws IOException {
+
+      BufferedReader br = null;
+      
+      try {
+         
+         br =new BufferedReader(new FileReader("agenda.csv"));
+         String line = br.readLine();
+         while (null!=line) {
+            String [] fields = line.split(SEPARATOR);
+            
+            Carpeta car = new Carpeta(fields[1],Integer.parseInt(fields[2]),fields[0]);
+            sec.insertarAlFinal(car);
+            line = br.readLine();
+         }
+         
+      } catch (Exception e) {
+         
+      } finally {
+         if (null!=br) {
+            br.close();
+         }
+      }
+      int a = sec.getContador();
+      for(int i = 0; i < a; i++){
+          System.out.println(sec.leerDato(i).toString());
+      } 
+}
+   
+}
