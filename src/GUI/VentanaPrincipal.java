423ffe0aef0776fd27bffde759c9100fa34fd2e9
AndersonVargas8 <anvargasa@unal.edu.co>
2021-07-15 16:35:02 -0500

    inicio gui

--- src/GUI/VentanaPrincipal.java
+++ src/GUI/VentanaPrincipal.java
@@ -1,4 +1,12 @@
 package GUI;
 
+import javax.swing.*;
+
 public class VentanaPrincipal {
+    private JPanel panel1;
+    private JTextArea textArea1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
